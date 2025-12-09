4311c62d4bd9c0bf142da5d35bee19188a9d340c
martinweb77 <145459004+martinweb77@users.noreply.github.com>
2025-12-10 00:39:52 +0100

    Create shoes.js

---
+++ src/data/shoes.js
@@ -0,0 +1,6 @@
+export const shoes = [
+  { id: "s1", name: "Zapatillas blancas", formality: ["informal"] },
+  { id: "s2", name: "Zapatillas beige urban", formality: ["informal"] },
+  { id: "s3", name: "Chelsea negro", formality: ["informal", "semiformal", "formal"] },
+  { id: "s4", name: "Chelsea marrón oscuro", formality: ["informal", "semiformal"] },
+];
