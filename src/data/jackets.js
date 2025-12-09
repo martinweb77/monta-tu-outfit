c07494bb0e7036d88198499af208c8f1b8a14713
martinweb77 <145459004+martinweb77@users.noreply.github.com>
2025-12-10 00:39:14 +0100

    Create jackets.js

---
+++ src/data/jackets.js
@@ -0,0 +1,7 @@
+export const jackets = [
+  { id: "j1", name: "Abrigo largo gris oscuro", formality: ["informal", "semiformal", "formal"] },
+  { id: "j2", name: "Abrigo largo negro", formality: ["informal", "semiformal", "formal"] },
+  { id: "j3", name: "Abrigo largo camel", formality: ["informal", "semiformal", "formal"] },
+  { id: "j4", name: "Gabardina beige", formality: ["informal", "semiformal", "formal"] },
+  { id: "j5", name: "Cazadora ante marrón oscuro", formality: ["informal", "semiformal"] },
+];
