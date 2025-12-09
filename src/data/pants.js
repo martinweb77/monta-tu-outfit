b1247dafbbb6ba0c9438e444f65016d8d3627065
martinweb77 <145459004+martinweb77@users.noreply.github.com>
2025-12-10 00:35:48 +0100

    Create pants.js

---
+++ src/data/pants.js
@@ -0,0 +1,14 @@
+export const pants = [
+  {
+    id: "p1",
+    name: "Vaquero slim gris oscuro",
+    formality: ["informal", "semiformal", "formal"],
+    color: "gris oscuro"
+  },
+  {
+    id: "p2",
+    name: "Vaquero negro skinny",
+    formality: ["informal", "semiformal"],
+    color: "negro"
+  }
+];
